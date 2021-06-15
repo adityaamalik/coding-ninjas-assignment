ea58f47cd6ffefbec10835cfe5992856335e06b3
Aditya Malik <aditya@Adityas-MacBook-Pro.local>
2021-06-15 22:04:06 +0530

    heading spacing changed

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -54,8 +54,13 @@ export const EventCol = styled(Col)`
 `;
 
 export const HeadingContainer = styled.div`
+  margin-bottom: 50px;
+  padding-top: 20px;
+
   @media only screen and (max-width: 768px) {
     padding: 15px;
+    padding-top: 50px;
+    margin-bottom: 20px;
   }
 `;
 
